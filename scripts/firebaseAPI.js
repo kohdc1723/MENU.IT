f81bd6f4ed36963d04ef3a920d0e36b95261eceb
Koh Dong-Chan <kohdc1723@gmail.com>
2021-12-02 10:14:47 -0800

    auth function code move

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -7,38 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "446911422013",
   appId: "1:446911422013:web:157cb7256eefcf66b15454"
 };
-  
-  //initialize database in firebaseAPI script
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  const storage = firebase.storage();
-
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        var profilename = document.getElementById('profile-name');
-        profilename.innerHTML = user.displayName;
-        profilename.addEventListener('click', ()=> {
-          window.location.href = "./profile.html";
-        })
-
-        var signout = document.getElementById('sign-out');
-        signout.innerHTML = 'Sign out';
-        signout.addEventListener('click', ()=> {
-          firebase.auth().signOut();
-          window.location.href = "./main.html";
-        })
-        
-    } else {
-        // No user is signed in.
-        var profilename = document.getElementById('profile-name');
-        profilename.innerHTML = "Sign In";
-        profilename.addEventListener('click', ()=> {
-          window.location.href = "./login.html";
-        })
-
-        var signout = document.getElementById('sign-out');
-        signout.style.visibility = 'hidden';
-        
-    }
-})
